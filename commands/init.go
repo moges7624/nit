09b75548806ab8eeb0a9449b54710f847ba539ba
moges <moges7624@gmail.com>
2026-03-30 21:24:08 +0300

    feat: init command

---
+++ commands/init.go
@@ -0,0 +1,54 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func Init(args []string) {
+	rootPath, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Couldn't get root path")
+		os.Exit(1)
+	}
+
+	path := "."
+
+	if len(args) > 0 {
+		path = args[0]
+	}
+
+	nitPath := filepath.Join(rootPath, path, ".git")
+
+	// create .git directory
+	err = os.MkdirAll(nitPath, 0o755)
+	if err != nil {
+		var pathError *os.PathError
+		if errors.As(err, &pathError) {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", pathError.Path, pathError.Err)
+		}
+
+		return
+	}
+
+	// create required subdirectories
+	dirs := []string{"objects", "refs/heads", "refs/tags"}
+	for _, dir := range dirs {
+		err = os.MkdirAll(filepath.Join(nitPath, dir), 0o755)
+		if err != nil {
+			fmt.Printf("Error creating %s: %v\n", dir, err)
+			os.Exit(1)
+		}
+	}
+
+	// Write the initial HEAD file
+	err = os.WriteFile(filepath.Join(nitPath, "HEAD"), []byte("ref: refs/heads/main\n"), 0o644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing HEAD: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Initialized empty nit repository in %s/\n", nitPath)
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/moges7624/nit/commands"
+)
+
+func main() {
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	switch cmd {
+	case "init":
+		commands.Init(args)
+	default:
+		fmt.Println(cmd)
+		fmt.Printf("nit: '%s' is not a nit command.\n", cmd)
+	}
+}
